8d41a5a2f95c79f0f7bc552ee817562bfde5001e
geloro94 <geloro94@gmail.com>
2022-11-10 11:19:58 +0100

    Fixed path in blaze-upload script

--- blaze-upload/sd_upload.sh
+++ blaze-upload/sd_upload.sh
@@ -19,12 +19,12 @@ done
 echo "Connected to Blaze@${BLAZE_SERVER_URL}"
 echo "$(python -m fhir_populator --endpoint ${BLAZE_SERVER_URL} --get-dependencies --non-interactive --only-put --package ${PACKAGES})"
 echo "Uploading own StructureDefinition instances to Blaze@${BALZE_SERVER_URL}"
-for file in -exec $(find ./profiles -name '*.json')
+for file in -exec $(find ./fhir_profiles -name '*.json')
 do
   curl -vX POST -d @"$file" -H "Content-Type: application/json" "${BLAZE_SERVER_URL}"
   echo "Uploading ${file}"
 done
-for file in -exec $(find ./profiles -name '*.xml')
+for file in -exec $(find ./fhir_profiles -name '*.xml')
 do
   curl -vX POST -d @"$file" -H "Content-Type: application/xml" "${BLAZE_SERVER_URL}"
   echo "Uploading ${file}"

aa8225c37bc6c376e5efa34f3e520a9a309b6e7d
Paul Behrend <paul.behrend@student.uni-luebeck.de>
2022-11-10 15:04:52 +0100

    Commiting local changes of upload.sh

--- blaze-upload/sd_upload.sh
+++ blaze-upload/sd_upload.sh
@@ -21,6 +21,7 @@ echo "$(python -m fhir_populator --endpoint ${BLAZE_SERVER_URL} --get-dependenci
 echo "Uploading own StructureDefinition instances to Blaze@${BALZE_SERVER_URL}"
 for file in -exec $(find ./profiles -name '*.json')
 do
+  echo "$file"
   curl -vX POST -d @"$file" -H "Content-Type: application/json" "${BLAZE_SERVER_URL}"
 done
 for file in -exec $(find ./profiles -name '*.xml')

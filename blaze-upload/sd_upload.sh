ea4df0e5a9a133f568db341c439d0044dcc7ccbd
Paul Behrend <paul.behrend@student.uni-luebeck.de>
2022-11-11 10:07:21 +0100

    Renamed blaze volume. Implemented Blaze healthcheck with new Blaze version 0.18.3

--- blaze-upload/sd_upload.sh
+++ blaze-upload/sd_upload.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-for i in $(seq 101)
-do
-  if [ "$i" -eq 101 ]
-  then
-    echo "Failed to connect to Blaze"
-    exit 1
-  fi
-  echo "Waiting for Blaze ($i/100)"
-  result=$(curl --fail "${BLAZE_SERVER_URL}" || exit 1)
-  exit_code=$?
-  echo "$result"
-  if [ "$exit_code" -eq 0 ]
-  then
-     break
-  fi
-  sleep 5
-done
-echo "Connected to Blaze@${BLAZE_SERVER_URL}"
+echo "Uploading profiles from Simplifier to Blaze@${BLAZE_SERVER_URL}"
 echo "$(python -m fhir_populator --endpoint ${BLAZE_SERVER_URL} --get-dependencies --non-interactive --only-put --package ${PACKAGES})"
 echo "Uploading own StructureDefinition instances to Blaze@${BALZE_SERVER_URL}"
 for file in -exec $(find ./fhir_profiles -name '*.json')
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 export "$(cat .env | xargs)"
 echo "Running test script"
 docker-compose -p feasibility-deploy -f docker-compose2.yml up
-echo "Tests concluded. Generated file can be found under $REPORT_LOCATION"
+echo "Tests concluded. Generated file can be found under ${REPORT_LOCATION}"

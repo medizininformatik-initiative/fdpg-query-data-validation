6b499bfd187b7ac9d00b8cfcb3b1987598161e4b
geloro94 <geloro94@gmail.com>
2022-11-18 12:00:04 +0100

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export $(grep -v '^#' .env | xargs)
-docker-compose -p "${PROJECT_CONTEXT}" -f docker-compose-validation.yml down
-docker-compose -p "${PROJECT_CONTEXT}" -f docker-compose-vms.yml down
+docker-compose -p "${PROJECT_CONTEXT}" -f docker-compose.yml down
 echo "Shutdown finished"

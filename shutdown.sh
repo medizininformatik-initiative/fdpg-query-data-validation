05a6fb008f356eaa9dfbba39240ffcd8032ea37b
Paul Behrend <paul.behrend@student.uni-luebeck.de>
2022-11-18 11:49:31 +0100

    Changed shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export "$(grep -v '^#' .env | xargs)"
-docker-compose -p "${PROJECT_CONTEXT}" -f docker-compose-validation.yml down
-docker-compose -p "${PROJECT_CONTEXT}" -f docker-compose-vms.yml down
+docker-compose -p "${PROJECT_CONTEXT}" -f validation_service/docker-compose-validation.yml down
+docker-compose -p "${PROJECT_CONTEXT}" -f validation_mapper_service/docker-compose-vms.yml down
 echo "Shutdown finished"

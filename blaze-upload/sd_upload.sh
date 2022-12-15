f82cb269873c5e45160d1c0d117045dbe27c0270
Paul Behrend <paul.behrend@student.uni-luebeck.de>
2022-12-15 10:13:51 +0100

    Refactoring fhir.py

--- blaze-upload/sd_upload.sh
+++ blaze-upload/sd_upload.sh
@@ -3,19 +3,19 @@ shopt -s globstar
 echo "Uploading profiles from Simplifier to Blaze@${BLAZE_SERVER_URL}"
 http_proxy=''
 https_proxy=''
-if [[ -z "${HTTP_PACKAGE_DOWNLOAD_PROXY}" ]]
+if [[ -n "${HTTP_PACKAGE_DOWNLOAD_PROXY}" ]]
 then
-  http_proxy="${HTTP_PACKAGE_DOWNLOAD_PROXY}"
+  http_proxy="--http-proxy ${HTTP_PACKAGE_DOWNLOAD_PROXY}"
 fi
-if [[ -z "${HTTPS_PACKAGE_DOWNLOAD_PROXY}" ]]
+if [[ -n "${HTTPS_PACKAGE_DOWNLOAD_PROXY}" ]]
 then
-  https_proxy="${HTTPS_PACKAGE_DOWNLOAD_PROXY}"
+  https_proxy="--https-proxy ${HTTPS_PACKAGE_DOWNLOAD_PROXY}"
 fi
-python -m fhir_populator --endpoint "${BLAZE_SERVER_URL}" --get-dependencies --non-interactive --only-put --http-proxy ${http_proxy} --https-proxy ${https_proxy} --package ${PACKAGES}
+python -m fhir_populator --endpoint "${BLAZE_SERVER_URL}" --get-dependencies --non-interactive --only-put ${http_proxy} ${https_proxy} --package ${PACKAGES}
 echo "Uploading own StructureDefinition instances to Blaze@${BALZE_SERVER_URL}"
 for file in ./fhir_profiles/**/*.json
 do
-  id="$(cat $file | jq -r '.id')"
+  id="$(cat "$file" | jq -r '.id')"
   curl -vX PUT -d @"$file" -H "Content-Type: application/json" "${BLAZE_SERVER_URL}/StructureDefinition/${id}"
   echo "Uploading ${file}"
 done
--- extraction/fhir.py
+++ extraction/fhir.py
@@ -24,15 +24,6 @@ class FHIRClient:
         if cert is not None and len(cert) > 0:
             self._cert = cert
 
-    def __make_request(self, url_string):
-        response = requests.get(url=url_string, headers=self._headers, proxies=self._proxies,
-                                verify=self._cert, auth=self._auth)
-        if response.status_code != 200:
-            raise HttpError(response.status_code, f"Paging failed with status code {response.status_code} and "
-                            f"headers {response.headers}:\n{response.text}")
-        else:
-            return response
-
     def get(self, resource_type, parameters=None, paging=True, get_all=False, max_cnt=sys.maxsize):
         assert resource_type in resource_types, f"The provided resource type '{resource_type}' has to be one of " \
                                                 f"{', '.join(resource_types)} "
@@ -41,7 +32,8 @@ class FHIRClient:
             param_string = "&".join([f"{k}={str(v)}" for k, v in parameters.items()])
             request_string = f"{request_string}?{param_string}"
         print(f"Requesting: {request_string} with headers {self._headers}")
-        response = self.__make_request(request_string)
+        response = make_request(request_string, headers=self._headers, proxies=self._proxies, auth=self._auth,
+                                cert=self._cert)
         bundle = json.loads(response.text)
         if not paging:
             return bundle
@@ -59,10 +51,10 @@ class FHIRClient:
 
 class PagingResult:
 
-    def __init__(self, bundle, max_cnt=sys.maxsize, headers=None, authorization=None, proxies=None, cert=None):
-        self.__current_page = bundle
-        self.__total = bundle.get('total', len(bundle.get('entry', [])))
-        self.__next_url = get_next_url(bundle)
+    def __init__(self, starting_url, max_cnt=sys.maxsize, headers=None, authorization=None, proxies=None, cert=None):
+        self.__current_page = None
+        self.__total = None  # bundle.get('total', len(bundle.get('entry', [])))
+        self.__next_url = starting_url  # get_next_url(bundle)
         self.__max_cnt = max_cnt
         self.__current_cnt = 0
         self.__headers = headers
@@ -77,6 +69,17 @@ class PagingResult:
     def __next__(self):
         # TODO: Implement better version of PagingResult which takes to initial request instead of the first bundle to
         # TODO: avoid convoluted condition checking
+        try:
+            self.__current_page = make_request(self.__next_url, headers=self.__headers, auth=self.__auth,
+                                               proxies=self.__proxies, cert=self.__cert)
+            self.__next_url = get_next_url(self.__current_page)
+            self.__current_cnt += len(self.__current_page.get('entry', []))
+            if self.__next_url is not None and self.__current_cnt < self.__max_cnt:
+        except HttpError as error:
+            raise StopIteration(f"Paging failed due to request failing") from error
+        except KeyError as error:
+            raise StopIteration(f"Paging failed due to missing element in bundle") from error
+
         if self.__stop:
             raise StopIteration
         self.__next_url = get_next_url(self.__current_page)
@@ -95,11 +98,10 @@ class PagingResult:
         return bundle
 
     def is_empty(self):
+        if self.__total is None:
+            # Ensure some statement can be made about the whether the bundle is empty
+            self.__next__()
         return self.__total == 0
-
-    # Might not be reliable depending on whether the server FHIR server returns a value for the total element
-    def get_total(self):
-        return self.__total
 
 
 def get_next_url(bundle):
@@ -113,6 +115,17 @@ def get_next_url(bundle):
         # print(f"Failed to retrieve bundle link")
         return None
     return next_url
+
+
+def make_request(url_string, headers, proxies, cert, auth, verify=True):
+    print(f"Requesting: {url_string}")
+    response = requests.get(url=url_string, headers=headers, proxies=proxies,
+                            cert=cert, auth=auth, verify=verify)
+    if response.status_code != 200:
+        raise HttpError(response.status_code, f"Request failed with status code {response.status_code} and "
+                        f"headers {response.headers}:\n{response.text}")
+    else:
+        return response
 
 
 class HttpError(ConnectionError):
